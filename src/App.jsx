94f63b4729ea1e673b3728637e9922f6b331ba11
CiprianBoamba <ciprian.boamba@gmail.com>
2025-01-24 15:27:04 +0200

    work on hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
+import { BrowserRouter } from 'react-router-dom';
+import { Hero } from './components';
+// import Hero from './components/Hero';
+
 import './App.css';
+import './index.css';
 
 const App = () => {
   return (
-    <>
-      <div>
-        personal website
-        <h1 class="text-3xl font-bold">Hello world!</h1>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Hero />
+        </div>
+        about
+        <div className="relative z-0">contact</div>
       </div>
-    </>
+    </BrowserRouter>
   );
 };
 
